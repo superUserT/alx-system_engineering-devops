d1bb06e24c3f7652c9f28bccc530225c32adb204
superUserT <rantshothabisomail@gmail.com>
2023-11-26 19:12:12 +0200

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,17 @@
-# puppet install flask
+# puppet_install_flask.pp
 
-package {'python3-pip':
+# Use a variable for the Flask version for easier maintenance
+$flask_version = '2.1.0'
+
+# Install python3-pip package
+package { 'python3-pip':
   ensure => present,
 }
 
+# Install Flask using pip3
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+  command => "/usr/bin/pip3 install flask==${flask_version}",
   path    => ['/usr/bin'],
-  creates => '/usr/local/lib/python3.8/dist-packages/flask/__init__.py',
+  unless  => "/usr/local/bin/flask --version | grep -q '${flask_version}'",
   require => Package['python3-pip'],
 }
